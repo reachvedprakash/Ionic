7d6352eb9d8d9b7f06d9d2a02a3e8b58e5dbda34
Ved Prakash <reachvedprakash@gmail.com>
2019-05-15 14:21:35 +0530

    Recipes Servise Added

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://thestayathomechef.com/wp-content/uploads/2018/03/German-Schnitzel-3-small.jpg',
+      ingredients: ['French Fries', 'Pork meat', 'Salad']
+
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
+      ingredients: ['spaghetti', 'Meat', 'Tomatoes']
+
+    },
+  ];
+
+
+  constructor() { }
+
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+        return recipe.id === recipeId;
+      })
+    };
+  }
+}
